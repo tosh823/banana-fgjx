daad86b296479d47f717b6ceef5e25245ad2bc53
Ilya Minyaev <ayli.veaynim@gmail.com>
2018-01-26 22:52:04 +0200

    Some stuff

---
+++ Assets/Scripts/Controllers/Movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Banana.Controllers {
+    public class Movement : MonoBehaviour {
+
+        public float Speed = 5f;
+
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+            float horInput = Input.GetAxis("Horizontal");
+            if (horInput > 0f) {
+                transform.Translate(transform.right * Speed * Time.deltaTime);
+            }
+            if (horInput < 0f) {
+                transform.Translate(-transform.right * Speed * Time.deltaTime);
+            }
+
+            float verInput = Input.GetAxis("Vertical");
+            if (verInput > 0f) {
+                transform.Translate(transform.up * Speed * Time.deltaTime);
+            }
+            if (verInput < 0f) {
+                transform.Translate(-transform.up * Speed * Time.deltaTime);
+
+            }
+        }
+    }
+}
