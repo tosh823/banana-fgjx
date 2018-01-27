2a258d945d3078a59f02775446f0841486dbf53c
Ilya Minyaev <ayli.veaynim@gmail.com>
2018-01-27 15:53:45 +0200

    Moving scenery

---
+++ Assets/Scripts/Controllers/Scenery.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Banana.Controllers {
+    public class Scenery : MonoBehaviour {
+
+        public float speed = 1f;
+
+        private CompositeCollider2D cc;
+        private float width;
+
+        // Use this for initialization
+        void Start() {
+            cc = GetComponent<CompositeCollider2D>();
+            width = cc.bounds.size.x;
+        }
+
+        // Update is called once per frame
+        void Update() {
+            if (transform.position.x < -width) {
+                Reposition();
+            }
+            else {
+                Vector3 step = transform.right * speed * Time.deltaTime;
+                transform.position -= step;
+            }
+        }
+
+        public void Reposition() {
+            Vector3 shifted = transform.position;
+            shifted.x = width;
+            transform.position = shifted;
+        }
+    }
+}
